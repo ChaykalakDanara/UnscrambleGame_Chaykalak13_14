eb680e4bcda8a79a6a52a24a6c7e6bd9b01bfc49
ChaykalakDanara <chaykalak.d08@gmail.com>
2026-04-23 20:17:26 +0300

    Initial commit

---
+++ app/src/main/java/com/chaykalak/unscramblegame13_14/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.chaykalak.unscramblegame13_14
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.chaykalak.unscramblegame13_14.ui.theme.UnscrambleGame13_14Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UnscrambleGame13_14Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    UnscrambleGame13_14Theme {
+        Greeting("Android")
+    }
+}
